e51ae55f3c526e06c067c904d9d2af10d40896d6
Borodin <servikb1@gmail.com>
2025-02-20 15:03:20 +0700

    Задание 2: Готово

--- Main.cpp
+++ Main.cpp
@@ -96,7 +96,34 @@ void task2(Menu m) {
 /// Даны два массива: А[M] и B[N] (M и N вводятся с клавиатуры). Необходимо создать третий массив минимально возможного размера, в котором нужно собрать элементы обоих массивов.
 /// </summary>
 void task3(Menu m) {
-
+	setCursorPosition(m.startPos);
+	int M = getValidInt(m, "Введите длину первого массива :");
+	nextLine(m.startPos);
+	int N = getValidInt(m, "Введите длину второго массива :");
+	nextLine(m.startPos, 2);
+	int* A = new int[M];
+	int* B = new int[N];
+	int randA = 0;
+	int randB = 10;
+	std::cout << "Было сгенерировано 2 массива случайными числами от "
+		<< randA << " до " << randB;
+	randIntArr(A, M, randA, randB);
+	randIntArr(B, N, randA, randB);
+	nextLine(m.startPos);
+	std::cout << "A = ";
+	show(A, M);
+	nextLine(m.startPos);
+	std::cout << "B = ";
+	show(B, N);
+	nextLine(m.startPos);
+	std::cout << "Получаем склеенный массив:";
+	int L = M + N;
+	int* C = joinArrays(A, M, B, N);
+	nextLine(m.startPos);
+	show(C, L);
+	delete[] A;
+	delete[] B;
+	delete[] C;
 	endTask(m);
 
 }
@@ -104,68 +131,21 @@ void task3(Menu m) {
 /// Даны два массива: А[M] и B[N] (M и N вводятся с клавиатуры). Необходимо создать третий массив минимально возможного размера, в котором нужно собрать общие элементы двух массивов без повторений.
 /// </summary>
 void task4(Menu m) {
-	srand(time(0));
-	int a = rand() % 100;
-	int b = rand() % 100;
-	int* pa = &a;
-	int* pb = &b;
-	setCursorPosition(m.startPos);
-	std::cout << "Я загадал 2 числа " << a <<" и " << b;
-	nextLine(m.startPos);
-	std::cout << "Проведя диагностику, я выяснил, что " << *findMax(pa, pb) << " больше чем " << *findMin(pa, pb);
+	
 	endTask(m);
 }
 /// <summary>
 /// Дан массив: А[M] (M вводится с клавиатуры). Необходимо удалить из массива четные или нечетные значения. Пользователь вводит данные в массив, а также с помощью меню решает, что нужно удалить.
 /// </summary>
 void task5(Menu m) {
-	Coordinate pos = m.startPos;
-	ColorANSI3b color;
-	char text[12];
-	char* pt = text;
-	setCursorPosition(pos);
-	while (true) {
-		std::cout << "Введите число: ";
-		std::cin >> text;
-		if (isNumber(text))
-			break;
-		else {
-			drawEmptyRectangle(16, 9, 3, m.cS.x - 17);
-			pos = m.startPos;
-			setCursorPosition(pos);
-			setColor(color.RedFG);
-			std::cout << "Число введено некорректно! Пожалуйста, попробуйте снова.";
-			resetColor();
-			nextLine(pos);
-		}
-	}
-	nextLine(pos);
-	std::cout << "Ваше число " << ((*pt == '-') ? "отрицательное" : "положительное");
-	m.startPos = pos;
+	
 	endTask(m);
 }
 /// <summary>
 /// Написать функцию, которая получает указатель на динамический массив и его размер. Функция должна удалить из массива все простые числа и вернуть указатель на новый динамический массив.
 /// </summary>
 void task6(Menu m) {
-	srand(time(0));
-	int a = rand() % 100;
-	int b = rand() % 100;
-	int* pa = &a;
-	int* pb = &b;
-	setCursorPosition(m.startPos);
-	std::cout << "Я загадал 2 числа a = " << *pa << " и  б = " << *pb;
-	nextLine(m.startPos);
-	std::cout << "Адрес числа a = " << pa << " и адрес б = " << pb;
-	nextLine(m.startPos, 2);
-	std::cout << "А теперь меняем их местами";
-	int temp = *pa;
-	*pa = *pb;
-	*pb = temp;
-	nextLine(m.startPos, 2);
-	std::cout << "Теперь значение числа A = " << *pa << " а  B = " << *pb;
-	nextLine(m.startPos);
-	std::cout << "Адрес числа A = " << pa << " и адрес B = " << pb;
+	
 	endTask(m);
 }
 /// <summary>
@@ -178,12 +158,13 @@ void task7(Menu m) {
 
 int main()
 {
+	gitPush("Задание 2: Готово");
 	system("chcp 1251>nul");
 
 
 
 	//startMenu();
 
-	gitPush("Задание 2: Готово");
+	
 }
 
